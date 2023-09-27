23043daab836b19949fd90123a0f3829795c154c
rayson <raysonyeap4c@gmail.com>
2023-09-27 13:59:32 +0800

    Change AddTask to throw Exception

--- src/main/java/seedu/address/logic/commands/AddTaskCommand.java
+++ src/main/java/seedu/address/logic/commands/AddTaskCommand.java
@@ -1,6 +1,10 @@
 package seedu.address.logic.commands;
 
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
+
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TASK_DESCRIPTION;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TASK_NAME;
 
 /**
  * Adds a task to the address book.
@@ -9,9 +13,23 @@ public class AddTaskCommand extends Command {
 
     public static final String COMMAND_WORD = "addTask";
 
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Adds a new task with the specified description.\n"
+            + "Parameters: "
+            + PREFIX_TASK_NAME + "NAME "
+            + PREFIX_TASK_DESCRIPTION + "DESCRIPTION "
+            + "Example: " + COMMAND_WORD + " "
+            + PREFIX_TASK_NAME + "Do laundry "
+            + PREFIX_TASK_DESCRIPTION + "Wash the clothes and pants in basket";
+
+    public static final String MESSAGE_NOT_IMPLEMENTED_YET =
+            "AddTask command not implemented yet";
+
+
     @Override
-    public CommandResult execute(Model model) {
-        return new CommandResult("Hello from addTask");
+    public CommandResult execute(Model model) throws CommandException {
+        throw new CommandException(MESSAGE_NOT_IMPLEMENTED_YET);
     }
+
 
 }
