5b4784bfab6d27ef538b958b8a41ac124b4d166f
Respirayson <raysonyeap4c@gmail.com>
2023-10-06 03:23:33 +0800

    Add JsonTaskListStorageTest

--- src/test/java/seedu/address/testutil/TypicalTasks.java
+++ src/test/java/seedu/address/testutil/TypicalTasks.java
@@ -1,13 +1,11 @@
 package seedu.address.testutil;
 
-import seedu.address.model.AddressBook;
-import seedu.address.model.TaskListBook;
-import seedu.address.model.person.Person;
-import seedu.address.model.task.Task;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+import seedu.address.model.TaskListBook;
+import seedu.address.model.task.Task;
 
 /**
  * A utility class containing a list of {@code Task} objects to be used in tests.
@@ -21,6 +19,16 @@ public class TypicalTasks {
     public static final Task TASK2 = new TaskBuilder()
             .withName("Read quant guide")
             .withDescription("The green book")
+            .build();
+
+    public static final Task TASK3 = new TaskBuilder()
+            .withName("Study cs3233")
+            .withDescription("Read Steven Halim Book for competitive programming")
+            .build();
+
+    public static final Task TASK4 = new TaskBuilder()
+            .withName("Plan for mods")
+            .withDescription("SEPSPEPESEP")
             .build();
 
     private TypicalTasks() {} // prevents instantiation
