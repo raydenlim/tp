a6c0e94b887a4c3463e8f829808fe548180cab0b
Respirayson <raysonyeap4c@gmail.com>
2023-10-06 01:19:09 +0800

    Add TaskNameContainsKeywordPredicateTest

--- src/main/java/seedu/address/model/task/TaskNameContainsKeywordsPredicate.java
+++ src/main/java/seedu/address/model/task/TaskNameContainsKeywordsPredicate.java
@@ -7,7 +7,7 @@ import seedu.address.commons.util.StringUtil;
 import seedu.address.commons.util.ToStringBuilder;
 
 /**
- * Tests that a {@code Person}'s {@code Name} matches any of the keywords given.
+ * Tests that a {@code Task}'s {@code Name} matches any of the keywords given.
  */
 public class TaskNameContainsKeywordsPredicate implements Predicate<Task> {
     private final List<String> keywords;
