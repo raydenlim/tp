4545ceee2261171f56cadda3156eae20996644d1
ldinghan <u.dinghan@gmail.com>
2023-10-13 14:35:02 +0800

    Add javadocs

--- src/main/java/seedu/address/logic/parser/CreateSessionCommandParser.java
+++ src/main/java/seedu/address/logic/parser/CreateSessionCommandParser.java
@@ -10,8 +10,18 @@ import seedu.address.logic.commands.session.CreateSessionCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.Name;
 
+/**
+ * Parses input arguments and creates a new CreateSessionCommand object.
+ */
 public class CreateSessionCommandParser implements Parser<CreateSessionCommand> {
 
+    /**
+     * Parses the given `args` string and returns a CreateSessionCommand object if the input is valid.
+     *
+     * @param args The input arguments to be parsed.
+     * @return A CreateSessionCommand object.
+     * @throws ParseException If the input arguments are not in the correct format or if parsing fails.
+     */
     @Override
     public CreateSessionCommand parse(String args) throws ParseException {
         ArgumentMultimap argMultimap =
@@ -29,6 +39,13 @@ public class CreateSessionCommandParser implements Parser<CreateSessionCommand> 
         return new CreateSessionCommand(sessionNumber, name);
     }
 
+    /**
+     * Checks if the specified prefixes are present in the given ArgumentMultimap.
+     *
+     * @param argumentMultimap The ArgumentMultimap to check for prefix presence.
+     * @param prefixes The prefixes to be checked.
+     * @return `true` if all specified prefixes are present, `false` otherwise.
+     */
     private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
         return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
     }
