db817c020945888ea42c43dc5869de380c1b8fec
wesho1107 <wesho1107@gmail.com>
2023-10-14 00:39:48 +0800

    Comment out JsonAdaptedConsultation to be implemented

--- src/main/java/seedu/address/storage/JsonAdaptedConsultation.java
+++ src/main/java/seedu/address/storage/JsonAdaptedConsultation.java
@@ -1,20 +1,20 @@
 package seedu.address.storage;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
+//import java.time.LocalDate;
+//import java.time.LocalTime;
+//import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.HashSet;
+//import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
+//import java.util.Set;
+//import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import seedu.address.commons.exceptions.IllegalValueException;
+//import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.model.consultation.Consultation;
-import seedu.address.model.person.Person;
+//import seedu.address.model.person.Person;
 
 /**
  * Jackson-friendly version of {@link Consultation}.
@@ -26,7 +26,7 @@ public class JsonAdaptedConsultation {
     private final List<JsonAdaptedPerson> students = new ArrayList<>();
 
     /**
-     * Constructs a {@code JsonAdaptedConsultation} with the given consultation details.
+     * * Constructs a {@code JsonAdaptedConsultation} with the given consultation details.
      */
     @JsonCreator
     public JsonAdaptedConsultation(@JsonProperty("date") String date, @JsonProperty("time") String time,
@@ -36,37 +36,37 @@ public class JsonAdaptedConsultation {
         this.students.addAll(students);
     }
 
-    /**
-     * Converts a given {@code Consultation} into this class for Jackson use.
-     */
-    public JsonAdaptedConsultation(Consultation source) {
-        date = source.getDate().toString();
-        time = source.getTime().toString();
-        students.addAll(source.getStudents().stream()
-                .map(JsonAdaptedPerson::new)
-                .collect(Collectors.toList()));
-    }
-
-    /**
-     * Converts this Jackson-friendly adapted session object into the model's {@code Session} object.
-     *
-     * @throws IllegalValueException if there were any data constraints violated in the adapted session.
-     */
-    public Consultation toModelType() throws IllegalValueException {
-        // TODO check for invalid values of date and time
-        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        LocalDate formattedDate = LocalDate.parse(date, dateFormatter);
-
-        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm");
-        LocalTime formattedTime = LocalTime.parse(time, timeFormatter);
-
-        final List<Person> studentsList = new ArrayList<>();
-        for (JsonAdaptedPerson student : students) {
-            studentsList.add(student.toModelType());
-        }
-        final Set<Person> studentSet = new HashSet<>(studentsList);
-
-        return new Consultation(formattedDate, formattedTime, studentSet);
-    }
+    //    /**
+    //     * Converts a given {@code Consultation} into this class for Jackson use.
+    //     */
+    //    public JsonAdaptedConsultation(Consultation source) {
+    //        date = source.getDate().toString();
+    //        time = source.getTime().toString();
+    //        students.addAll(source.getStudents().stream()
+    //                .map(JsonAdaptedPerson::new)
+    //                .collect(Collectors.toList()));
+    //    }
+    //
+    //    /**
+    //     * Converts this Jackson-friendly adapted session object into the model's {@code Session} object.
+    //     *
+    //     * @throws IllegalValueException if there were any data constraints violated in the adapted session.
+    //     */
+    //    public Consultation toModelType() throws IllegalValueException {
+    //        // TODO check for invalid values of date and time
+    //        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    //        LocalDate formattedDate = LocalDate.parse(date, dateFormatter);
+    //
+    //        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm");
+    //        LocalTime formattedTime = LocalTime.parse(time, timeFormatter);
+    //
+    //        final List<Person> studentsList = new ArrayList<>();
+    //        for (JsonAdaptedPerson student : students) {
+    //            studentsList.add(student.toModelType());
+    //        }
+    //        final Set<Person> studentSet = new HashSet<>(studentsList);
+    //
+    //        return new Consultation(formattedDate, formattedTime, studentSet);
+    //    }
 
 }
