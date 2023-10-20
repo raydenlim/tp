aea1f472336141e8323497e1a44f9c0e683a4d34
Originalidk <ryanpang321@gmail.com>
2023-10-20 13:39:44 +0800

    Remove tag from JsonAdaptedPerson second constructor

--- src/main/java/seedu/address/storage/JsonAdaptedPerson.java
+++ src/main/java/seedu/address/storage/JsonAdaptedPerson.java
@@ -54,9 +54,9 @@ class JsonAdaptedPerson {
         this.assignmentMap = assignmentMap;
     }
 
-    public JsonAdaptedPerson(@JsonProperty("name") String name, @JsonProperty("phone") String phone,
-                             @JsonProperty("email") String email, @JsonProperty("address") String address,
-                             @JsonProperty("tags") List<JsonAdaptedTag> tags) {
+    public JsonAdaptedPerson(String name, String phone,
+                             String email, String address,
+                             List<JsonAdaptedTag> tags) {
         this.name = name;
         this.phone = phone;
         this.email = email;
--- src/main/java/seedu/address/storage/JsonSerializableAssignmentMap.java
+++ src/main/java/seedu/address/storage/JsonSerializableAssignmentMap.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import com.fasterxml.jackson.annotation.JsonRootName;
 import seedu.address.model.ReadOnlyAssignmentMap;
 import seedu.address.model.person.assignment.AssignmentName;
 import seedu.address.model.person.assignment.initialise.AssignmentInitialise;
@@ -12,6 +13,7 @@ import seedu.address.model.person.assignment.initialise.AssignmentInitialise;
 /**
  * An Immutable AssignmentMap that is serializable to JSON format.
  */
+@JsonRootName("assignmentMap")
 public class JsonSerializableAssignmentMap {
 
     private final HashMap<String, JsonAdaptedAssignment> assignments;
