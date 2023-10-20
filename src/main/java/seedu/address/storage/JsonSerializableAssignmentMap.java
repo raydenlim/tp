d0b48bc464295af6080c80280008b20922293413
Originalidk <ryanpang321@gmail.com>
2023-10-20 13:42:18 +0800

    Fix checkstyle error

--- src/main/java/seedu/address/storage/JsonSerializableAssignmentMap.java
+++ src/main/java/seedu/address/storage/JsonSerializableAssignmentMap.java
@@ -4,8 +4,8 @@ import java.util.HashMap;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-
 import com.fasterxml.jackson.annotation.JsonRootName;
+
 import seedu.address.model.ReadOnlyAssignmentMap;
 import seedu.address.model.person.assignment.AssignmentName;
 import seedu.address.model.person.assignment.initialise.AssignmentInitialise;
