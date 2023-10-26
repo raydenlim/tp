7ec7f3fb29823182cc26a3e010fb1e9b303628a2
Originalidk <ryanpang321@gmail.com>
2023-10-26 14:37:50 +0800

    Add deletecomment feature with tests

--- src/main/java/seedu/address/logic/commands/DeleteCommentCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteCommentCommand.java
@@ -1,6 +1,7 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ASSIGNMENT;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
@@ -19,34 +20,33 @@ import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
 import seedu.address.model.person.assignment.AssignmentMap;
 import seedu.address.model.person.assignment.AssignmentName;
-import seedu.address.model.person.assignment.Grade;
+import seedu.address.model.person.assignment.Comment;
 import seedu.address.model.tag.Tag;
 
 /**
- * Deletes the grade from a person's assignment.
+ * Deletes the comment from a person's assignment.
  */
-public class DeleteGradeCommand extends Command {
+public class DeleteCommentCommand extends Command {
 
-    public static final String COMMAND_WORD = "deletegrade";
+    public static final String COMMAND_WORD = "deletecomment";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Deletes a grade from a person’s assignment identified "
-            + "by the index number used in the displayed person list. "
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Deletes the comment from a person’s assignment "
+            + "identified by the index number used in the displayed person list. "
             + "Parameters: INDEX (must be a positive integer) "
             + PREFIX_ASSIGNMENT + "ASSIGNMENT ";
 
-    public static final String MESSAGE_SUCCESS = "Deleted grade from assignment: %1$s";
+    public static final String MESSAGE_SUCCESS = "Deleted comment from assignment: %1$s";
 
-    public static final String MESSAGE_CONSTRAINT = "Cannot delete the grade from an ungraded assignment";
+    public static final String MESSAGE_CONSTRAINT = "Cannot delete the comment from an assignment with no comment";
 
     private final AssignmentName assignmentName;
     private final Index index;
 
     /**
-     * Creates a DeleteGradeCommand to delete the grade from a person's assignment.
+     * Creates a DeleteCommentCommand to delete the comment from a person's assignment.
      */
-    public DeleteGradeCommand(Index index, AssignmentName assignmentName) {
-        requireNonNull(index);
-        requireNonNull(assignmentName);
+    public DeleteCommentCommand(Index index, AssignmentName assignmentName) {
+        requireAllNonNull(index, assignmentName);
         this.index = index;
         this.assignmentName = assignmentName;
     }
@@ -67,12 +67,12 @@ public class DeleteGradeCommand extends Command {
         Person personToEdit = lastShownList.get(index.getZeroBased());
 
         AssignmentMap assignmentMapReference = personToEdit.getAllAssignments();
-        boolean isGraded = assignmentMapReference.get(this.assignmentName).gradingStatus();
-        if (!isGraded) {
-            throw new CommandException(DeleteGradeCommand.MESSAGE_CONSTRAINT);
+        boolean isCommentedOn = assignmentMapReference.get(this.assignmentName).commentStatus();
+        if (!isCommentedOn) {
+            throw new CommandException(DeleteCommentCommand.MESSAGE_CONSTRAINT);
         }
 
-        Person editedPerson = createUngradedPerson(personToEdit);
+        Person editedPerson = createRemovedCommentPerson(personToEdit);
 
         model.setPerson(personToEdit, editedPerson);
         model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
@@ -80,21 +80,20 @@ public class DeleteGradeCommand extends Command {
     }
 
     /**
-     * Creates a new Person with the ungraded assignment.
+     * Creates a new Person with the comment on an assignment removed.
      *
      * @param reference The person to be graded.
-     * @return New person with a graded assignment.
+     * @return New person with the comment on an assignment removed.
      */
-    public Person createUngradedPerson(Person reference) {
+    public Person createRemovedCommentPerson(Person reference) {
         Name name = reference.getName();
         Phone phone = reference.getPhone();
         Email email = reference.getEmail();
         Address address = reference.getAddress();
         Set<Tag> tags = reference.getTags();
         Set<GradedTest> gradedTest = reference.getGradedTest();
-        Grade ungraded = reference.getAllAssignments().get(this.assignmentName).getGrade().ungrade();
         AssignmentMap updatedAssignmentMap =
-                reference.getAllAssignments().createUpdatedMap(this.assignmentName, ungraded);
+                reference.getAllAssignments().createUpdatedMap(this.assignmentName, new Comment());
         return new Person(name, phone, email, address, tags, updatedAssignmentMap, gradedTest);
     }
 
@@ -105,14 +104,14 @@ public class DeleteGradeCommand extends Command {
         }
 
         // instanceof handles nulls
-        if (!(other instanceof DeleteGradeCommand)) {
+        if (!(other instanceof DeleteCommentCommand)) {
             return false;
         }
 
-        DeleteGradeCommand otherDeleteGradeCommand = (DeleteGradeCommand) other;
+        DeleteCommentCommand otherDeleteCommentCommand = (DeleteCommentCommand) other;
 
-        boolean isSameAssignmentName = this.assignmentName.equals(otherDeleteGradeCommand.assignmentName);
-        boolean isSamePersonIndex = this.index.equals(otherDeleteGradeCommand.index);
+        boolean isSameAssignmentName = this.assignmentName.equals(otherDeleteCommentCommand.assignmentName);
+        boolean isSamePersonIndex = this.index.equals(otherDeleteCommentCommand.index);
 
         return isSameAssignmentName && isSamePersonIndex;
     }
---
+++ src/test/java/seedu/address/logic/commands/DeleteCommentCommandTest.java
@@ -0,0 +1,144 @@
+package seedu.address.logic.commands;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_ASSIGNMENT_NAME;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.logic.commands.CommandTestUtil.showPersonAtIndex;
+import static seedu.address.testutil.TypicalAssignments.ASSIGNMENT1;
+import static seedu.address.testutil.TypicalComments.COMMENT1;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.Messages;
+import seedu.address.model.AddressBook;
+import seedu.address.model.ConsultationListBook;
+import seedu.address.model.GradedTestListBook;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.SessionListBook;
+import seedu.address.model.TaskListBook;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.assignment.AssignmentMap;
+import seedu.address.model.person.assignment.AssignmentName;
+import seedu.address.testutil.PersonBuilder;
+
+public class DeleteCommentCommandTest {
+
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs(), new TaskListBook(),
+            new SessionListBook(), new ConsultationListBook(), new GradedTestListBook());
+
+    @Test
+    public void execute_deleteGrade_success() {
+        Index targetIndex = INDEX_FIRST_PERSON;
+        List<Person> lastShownList = model.getFilteredPersonList();
+        Person personToEdit = lastShownList.get(targetIndex.getZeroBased());
+        AssignmentMap assignments =
+                personToEdit.getAllAssignments().createUpdatedMap(ASSIGNMENT1.getName(), COMMENT1);
+        personToEdit = new PersonBuilder(personToEdit, assignments).build();
+        model.setPerson(model.getFilteredPersonList().get(0), personToEdit);
+
+
+        AssignmentName assignmentName = ASSIGNMENT1.getName();
+        DeleteCommentCommand deleteCommentCommand = new DeleteCommentCommand(targetIndex, assignmentName);
+        Person editedPerson = deleteCommentCommand.createRemovedCommentPerson(personToEdit);
+
+        String expectedMessage = String.format(DeleteCommentCommand.MESSAGE_SUCCESS, assignmentName);
+
+        Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()),
+                new UserPrefs(), new TaskListBook(), new SessionListBook(),
+                new ConsultationListBook(), new GradedTestListBook());
+        expectedModel.setPerson(model.getFilteredPersonList().get(0), editedPerson);
+
+        assertCommandSuccess(deleteCommentCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_ungradedAssignment_failure() {
+        Index targetIndex = INDEX_FIRST_PERSON;
+        List<Person> lastShownList = model.getFilteredPersonList();
+        Person personToEdit = lastShownList.get(targetIndex.getZeroBased());
+
+        AssignmentName assignmentName = new AssignmentName(VALID_ASSIGNMENT_NAME);
+        DeleteCommentCommand deleteCommentCommand = new DeleteCommentCommand(targetIndex, assignmentName);
+        Person editedPerson = deleteCommentCommand.createRemovedCommentPerson(personToEdit);
+
+        Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()),
+                new UserPrefs(), new TaskListBook(), new SessionListBook(),
+                new ConsultationListBook(), new GradedTestListBook());
+        expectedModel.setPerson(model.getFilteredPersonList().get(0), editedPerson);
+
+        assertCommandFailure(deleteCommentCommand, model, DeleteCommentCommand.MESSAGE_CONSTRAINT);
+    }
+
+    @Test
+    public void execute_invalidPersonIndexUnfilteredList_failure() {
+        Index outOfBoundIndex = Index.fromOneBased(model.getFilteredPersonList().size() + 1);
+        AssignmentName assignmentName = new AssignmentName(VALID_ASSIGNMENT_NAME);
+        DeleteCommentCommand deleteCommentCommand = new DeleteCommentCommand(outOfBoundIndex, assignmentName);
+
+        assertCommandFailure(deleteCommentCommand, model, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+    }
+
+    /**
+     * Edit filtered list where index is larger than size of filtered list,
+     * but smaller than size of address book
+     */
+    @Test
+    public void execute_invalidPersonIndexFilteredList_failure() {
+        showPersonAtIndex(model, INDEX_FIRST_PERSON);
+        Index outOfBoundIndex = INDEX_SECOND_PERSON;
+        // ensures that outOfBoundIndex is still in bounds of address book list
+        assertTrue(outOfBoundIndex.getZeroBased() < model.getAddressBook().getPersonList().size());
+        AssignmentName assignmentName = new AssignmentName(VALID_ASSIGNMENT_NAME);
+        DeleteCommentCommand deleteCommentCommand = new DeleteCommentCommand(outOfBoundIndex, assignmentName);
+
+        assertCommandFailure(deleteCommentCommand, model, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+    }
+
+    @Test
+    public void equal_sameObject_success() {
+        AssignmentName assignmentName = new AssignmentName(VALID_ASSIGNMENT_NAME);
+        DeleteCommentCommand deleteCommentCommand = new DeleteCommentCommand(INDEX_FIRST_PERSON, assignmentName);
+
+        assertEquals(deleteCommentCommand, deleteCommentCommand);
+    }
+
+    @Test
+    public void equals_differentObjectType_failure() {
+        AssignmentName assignmentName = new AssignmentName(VALID_ASSIGNMENT_NAME);
+        DeleteCommentCommand deleteCommentCommand = new DeleteCommentCommand(INDEX_FIRST_PERSON, assignmentName);
+
+        assertFalse(deleteCommentCommand.equals(5));
+    }
+
+    @Test
+    public void equals_differentObjectSameContent_success() {
+        AssignmentName assignmentName = new AssignmentName(VALID_ASSIGNMENT_NAME);
+        DeleteCommentCommand deleteCommentCommandFirst = new DeleteCommentCommand(INDEX_FIRST_PERSON, assignmentName);
+        DeleteCommentCommand deleteCommentCommandSecond = new DeleteCommentCommand(INDEX_FIRST_PERSON, assignmentName);
+
+        assertEquals(deleteCommentCommandFirst, deleteCommentCommandSecond);
+    }
+
+    @Test
+    public void equals_differentObjectDifferentContent_failure() {
+        AssignmentName assignmentNameFirst = new AssignmentName(VALID_ASSIGNMENT_NAME);
+        AssignmentName assignmentNameSecond = ASSIGNMENT1.getName();
+        DeleteCommentCommand deleteCommentCommandFirst =
+            new DeleteCommentCommand(INDEX_FIRST_PERSON, assignmentNameFirst);
+        DeleteCommentCommand deleteCommentCommandSecond =
+            new DeleteCommentCommand(INDEX_FIRST_PERSON, assignmentNameSecond);
+
+        assertFalse(deleteCommentCommandFirst.equals(deleteCommentCommandSecond));
+    }
+}
