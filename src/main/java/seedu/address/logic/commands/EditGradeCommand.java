26232d966406a024e3095294b247dbd9cf642b8a
Originalidk <ryanpang321@gmail.com>
2023-10-27 02:58:16 +0800

    Fix createUpdatedMap bug

--- src/main/java/seedu/address/logic/commands/EditGradeCommand.java
+++ src/main/java/seedu/address/logic/commands/EditGradeCommand.java
@@ -63,7 +63,7 @@ public class EditGradeCommand extends Command {
             throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
         }
 
-        if (!AssignmentName.isValidName(this.assignmentName.toString())) {
+        if (!AssignmentName.isValidName(this.assignmentName)) {
             throw new CommandException(AssignmentName.MESSAGE_CONSTRAINTS);
         }
 
